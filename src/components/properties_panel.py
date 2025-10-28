35e1acb6664369846a71daf71910cf0fcb635a52
zhangzhou <zhou.zhang@trinapower.com>
2025-10-28 14:09:00 +0800

    feat(properties_panel): 添加新的变压器规格选项
    在变压器属性面板中新增110/10 kV和10/0.4 kV的变压器规格选项，以满足更多使用场景的需求

--- src/components/properties_panel.py
+++ src/components/properties_panel.py
@@ -546,8 +546,9 @@ class PropertiesPanel(QWidget):
                     'choices': [
                         '160 MVA 380/110 kV', '100 MVA 220/110 kV',
                         '63 MVA 110/20 kV', '40 MVA 110/20 kV', '25 MVA 110/20 kV',
+                        '63 MVA 110/10 kV', '40 MVA 110/10 kV', '25 MVA 110/10 kV',
                         '0.25 MVA 20/0.4 kV', '0.4 MVA 20/0.4 kV', '0.63 MVA 20/0.4 kV',
-                        '1.0 MVA 20/0.4 kV', '1.6 MVA 20/0.4 kV'
+                        '0.25 MVA 10/0.4 kV', '0.4 MVA 10/0.4 kV', '0.63 MVA 10/0.4 kV'
                     ], 
                     'default': '25 MVA 110/20 kV'
                 },
